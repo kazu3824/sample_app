93e7c706c2d5bf07f95b6b5b3f876894737e933b
澤西一樹 <sawanishiichiju@sawanishiichijunoMacBook-Air.local>
2022-09-01 02:51:51 +0900

    ruby学習

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
   has_one_attached :image
+  has_many :post_images, dependent: :destroy
 
   def get_image
     (profile_image.attached?) ? profile_image : "no_image.jpg"
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "homes#top"
+  namespace :public do
+    resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  end
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # 顧客用
   devise_for :users,controllers: {
     registrations: "public/registrations",
