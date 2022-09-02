c5dc3f74dfc0a09796cea05cd656b6e2a2fc706d
澤西一樹 <sawanishiichiju@sawanishiichijunoMacBook-Air.local>
2022-09-02 18:10:33 +0900

    userエラー解消＝原因imagemagick

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   namespace :public do
     resources :post_images, only: [:new, :create, :index, :show, :destroy]
+    resources :users, only: [:show, :edit]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # 顧客用
