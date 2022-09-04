6638fc0c9b463aa98c61601dc276f2b867bc84b6
澤西一樹 <sawanishiichiju@sawanishiichijunoMacBook-Air.local>
2022-09-04 17:45:30 +0900

    アプリケーションを完成させよう２復習完了

--- app/controllers/public/post_images_controller.rb
+++ app/controllers/public/post_images_controller.rb
@@ -4,11 +4,11 @@ class Public::PostImagesController < ApplicationController
   end
 
   def index
-    @post_images =  PostImage.all
+    @post_images = PostImage.all
   end
 
   def show
-    @post_image = PostImage.find(params[:id]) 
+    @post_image = PostImage.find(params[:id])
     @post_comment = PostComment.new
   end
 
--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,6 +1,6 @@
 class PostImage < ApplicationRecord
   has_one_attached :image 
-  belongs_to :user, optional: true
+  belongs_to :user
   has_many :post_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
